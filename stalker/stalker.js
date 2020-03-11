b514acb7b9b19eec6714fc59980ef46a1eca21b1
Saksham Garg <55405810+sakshusakshusakshu@users.noreply.github.com>
2020-03-11 17:38:27 +0530

    Add files via upload

---
+++ stalker/stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first Name ?");
+var lastName = prompt("What is your last Name ?");
+var age = prompt("What is your age ?");
+console.log("Your full name is : " +firstName +' '+ +lastName);
+console.log("Your age is " +age);
